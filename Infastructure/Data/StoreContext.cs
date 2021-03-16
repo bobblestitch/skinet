360a0b0740cb1d11c459a654b496630e0581634d
bobblestitch <shurtliff.sandbox@gmail.com>
2021-03-16 15:57:19 -0600

    Initial Commit

---
+++ Infastructure/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+
+namespace Infastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
